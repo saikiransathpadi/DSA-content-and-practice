5161a055aebeeebdca2b07777d35e0765a8f7ab4
saikiran sathpadi <sathpadisaikiran@gmail.com>
2024-08-16 14:08:10 +0530

    LC 48

--- LeetCode/2. Arrays ll/lc_48_rotate_image.py
+++ LeetCode/2. Arrays ll/lc_48_rotate_image.py
@@ -6,4 +6,26 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+
+        # Transpose
+        for i in range(len(matrix)):
+            for j in range(i):
+                # print(matrix)
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
+        print(matrix)
+        for i in matrix:
+            self.reverse(i)
+        
+        print(matrix)
+
+    def reverse(self, arr):
+        i = 0
+        j = len(arr)-1
+        while i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+            j -= 1
+
+
+Solution().rotate([[1,2,3],[4,5,6],[7,8,9]])
