fa59114f822da5011d8462e8cb9f958679b395d7
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-07-16 12:44:59 +0530

    LC: 3201 - Daily challenge

---
+++ LeetCode/daily_streak/3201_Find_the_Maximum_Length_of_Valid_Subsequence_I.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def maximumLength(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) < 3: return len(nums)
+        for i in range(len(nums)):
+            nums[i] = nums[i] % 2
+
+        count1 = 0
+        for i in nums:
+            if i == 1:
+                count1 += 1
+
+        count0 = 0
+        for i in nums:
+            if i == 0:
+                count0 += 1
+        
+        countalt = 1
+
+        j = 0
+        while j < len(nums):
+            if nums[j] == 1:
+                j += 1
+                while j < len(nums) and nums[j] !=0:
+                    j += 1
+                if j < len(nums) and nums[j] == 0:
+                    countalt += 1
+            elif nums[j] == 0:
+                j += 1
+                while j < len(nums) and nums[j] !=1:
+                    j += 1
+                if j < len(nums) and nums[j] == 1:
+                    countalt += 1
+        print(count0, count1, countalt, nums)
+        return max(count0, count1, countalt)
+
+
+input = [4,2,6]
+print(Solution().maximumLength(input))
