18b8f787a0ba4f3ce3258348415d49fa8762e905
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-07-17 17:27:10 +0530

    3202: Maintain Streak - Revisits

---
+++ LeetCode/daily_streak/3202_Find_the_Maximum_Length_of_Valid_Subsequence_II.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def maximumLength(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        dp = [[0] * k for _ in range(k)]
+        res = 0
+        for num in nums:
+            num %= k
+            for prev in range(k):
+                dp[prev][num] = dp[num][prev] + 1
+                res = max(res, dp[prev][num])
+        return res
---
+++ personal.md
@@ -0,0 +1,3 @@
+# Revisits
+
+- LeetCode/daily_streak/3202_Find_the_Maximum_Length_of_Valid_Subsequence_II.py - DP
