51a9fbb5eb8228718b8a27da034c561e10e91aab
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-07-30 17:08:01 +0530

    LC: 21 Optimized

--- LeetCode/Blind75/21_Merge_Two_Sorted_Lists.py
+++ LeetCode/Blind75/21_Merge_Two_Sorted_Lists.py
@@ -3,32 +3,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        res = []
+    def mergeTwoLists(self, list1, list2):
+        res = ListNode()
+        dum = res
         while list1 and list2:
-            if list1.val < list2.val:
-                res.append(list1.val)
-                list1 = list1.next
-            else:
-                res.append(list2.val)
+            if list1.val > list2.val:
+                dum.next = list2
                 list2 = list2.next
-        while list1:
-            res.append(list1.val)
-            list1 = list1.next
-        while list2:
-            res.append(list2.val)
-            list2 = list2.next
-        
-        head = None
-        temp = head
-        for i in res:
-            if temp is None:
-                temp = ListNode(i)
-                head = temp
             else:
-                temp.next = ListNode(i)
-                temp = temp.next
-        return head
-        
-        
+                dum.next = list1
+                list1 = list1.next
+            dum = dum.next
+        if list1:
+            dum.next = list1
+        if list2:
+            dum.next = list2
+        return res.next
