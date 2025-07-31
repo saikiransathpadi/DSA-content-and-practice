251a89593e739c2c5e8520488b87324414176f61
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-07-31 12:03:36 +0530

    572: LC - Blind 75

---
+++ LeetCode/Blind75/572_Subtree_of_Another_Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: Optional[TreeNode]
+        :type subRoot: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not root or not subRoot:
+            return False
+        if root.val == subRoot.val and self.isSameTree(root, subRoot):
+            return True
+        else:
+            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    def isSameTree(self, root1, root2):
+        if not root1 and not root2:
+            return True
+        if (root1 and not root2) or (root2 and not root1):
+            return False
+        if root1.val == root2.val:
+            return self.isSameTree(root1.left, root2.left) and self.isSameTree(root1.right, root2.right)
+        else:
+            return False
