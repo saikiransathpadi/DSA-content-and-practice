c0ba5c72664b933658e937ebc816ee2f68f6f2d5
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-08-05 22:04:18 +0530

    LC: 19 - Blind 75

---
+++ LeetCode/Blind75/19_Remove_Nth_Node_From_End_of_List.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: Optional[ListNode]
+        :type n: int
+        :rtype: Optional[ListNode]
+        """
+        temp=head
+        offset = ListNode()
+        offset.next = head
+        head = offset
+        for i in range(n-1):
+            temp = temp.next
+        
+        while temp and temp.next:
+            temp = temp.next
+            offset = offset.next
+
+        offset.next = offset.next.next if offset.next else None
+        return head.next
