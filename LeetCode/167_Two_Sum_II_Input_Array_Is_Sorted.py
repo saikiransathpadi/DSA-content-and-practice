160477433855b0dea993ba6ffaf28cdb57c28c2f
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-08-05 19:16:43 +0530

    167: LC - prereq of 15

---
+++ LeetCode/167_Two_Sum_II_Input_Array_Is_Sorted.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        i = 0
+        j = len(numbers)-1
+        while i < j:
+            if numbers[i] + numbers[j] == target:
+                return [i+1, j+1]
+            elif numbers[i] + numbers[j] > target:
+                j -= 1
+            else:
+                i += 1
+
+
+print(Solution().twoSum([2,7,11,15], 9))
