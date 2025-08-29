55d544580b98117610429794d226085d62e68372
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-08-29 15:36:55 +0530

    235: LC - Blind 75 (Lowest Common Ancestor of a Binary Search Tree)

---
+++ LeetCode/Blind75/235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val > q.val:
+            p,q = q, p
+        if not root: return None
+        if root.val == p.val or root.val == q.val or (p.val < root.val < q.val):
+            return root
+        
+        if root.val < p.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return self.lowestCommonAncestor(root.left, p, q)
