6f80c86b11c95861e66c4f7a2a6fec901e1666a5
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-09-04 16:36:40 +0530

    323: LC - Blind 75 (Number of Connected Components in an Undirected Graph)

---
+++ LeetCode/Blind75/323_Number_of_Connected_Components_in_an_Undirected_Graph.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        total = 0
+
+        visited = set()
+
+        nei_hash = {i: [] for i in range(n)}
+
+        for i in edges:
+            nei_hash[i[0]].append(i[1])
+            nei_hash[i[1]].append(i[0])
+
+        def dfs(cur, prev):
+            if cur in visited:
+                return
+
+            visited.add(cur)
+            for c in nei_hash[cur]:
+                if c != prev:
+                    dfs(c, cur)
+        
+        for i in range(n):
+            if i not in visited:
+                total +=1
+                dfs(i, -1)
+        return total
+            
+
+
+n=3
+edges=[[0,1],[0,2],[1,2]]
+print(Solution().countComponents(n, edges))
