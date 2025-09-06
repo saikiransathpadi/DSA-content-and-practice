37a875eb3d7d4762fee9f68e9cbb87322daf5ff9
saikiransathpadi <sathpadisaikiran@gmail.com>
2025-09-06 13:50:34 +0530

    417: LC - Blind 75 - (Pacific Atlantic Water Flow)

---
+++ LeetCode/Blind75/417_Pacific_Atlantic_Water_Flow.py
@@ -0,0 +1,37 @@
+from typing import List 
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(heights), len(heights[0])
+        pac, atl = set(), set()
+
+        def dfs(r, c, visit, prev_hei):
+            if (r, c) in visit or r < 0 or c < 0 or r >= rows or c >= cols or heights[r][c] < prev_hei:
+                return
+            visit.add((r, c))
+
+            dfs(r-1, c, visit, heights[r][c])
+            dfs(r+1, c, visit, heights[r][c])
+            dfs(r, c-1, visit, heights[r][c])
+            dfs(r, c+1, visit, heights[r][c])
+
+        for c in range(cols):
+            dfs(0, c, pac, heights[0][c])
+            dfs(rows-1, c, atl, heights[rows-1][c])
+        
+        for r in range(rows):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, cols-1, atl, heights[r][cols-1])
+        
+        res = []
+
+        for i in range(rows):
+            for j in range(cols):
+                if (i, j) in atl and (i, j) in pac:
+                    res.append([i, j])
+        return res
+
+
+
+heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+print(Solution().pacificAtlantic(heights))
